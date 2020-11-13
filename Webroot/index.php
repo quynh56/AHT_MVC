3d3c8f42b26d034db20f9ef64b163287fbd4f411
quynh dang <quyynhdaang@gmail.com>
2020-11-13 17:21:21 +0700

    viet hoa ten file

--- Webroot/index.php
+++ Webroot/index.php
@@ -6,7 +6,7 @@ use AHT_MVC1\Config\Core;
 use AHT_MVC1\Request;
 use AHT_MVC1\Dispatcher;
 
-require __DIR__ . '..\..\vendor\autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"]));
 define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
